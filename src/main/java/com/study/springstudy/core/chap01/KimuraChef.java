16a80e5602d8ef6e029e4b5313f8d616a0d29bae
kimbyeongmo <kbmomo9999@naver.com>
2024-05-14 15:58:45 +0900

    프론트 컨트롤러 패턴 part 1 and 2

---
+++ src/main/java/com/study/springstudy/core/chap01/KimuraChef.java
@@ -0,0 +1,10 @@
+package com.study.springstudy.core.chap01;
+
+import ch.qos.logback.core.joran.spi.NoAutoStartUtil;
+
+public class KimuraChef {
+
+    public void cooking(){
+        System.out.println("스시의 장인 키무라다.");
+    }
+}
