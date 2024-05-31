fe6a2869b0ae557fd9322762883b83bd5e7ccd05
kimbyeongmo <kbmomo9999@naver.com>
2024-05-31 15:34:09 +0900

    # 내가 쓴 글에만 삭제버튼 조건부 렌더링하기

--- src/main/java/com/study/springstudy/springmvc/util/LoginUtil.java
+++ src/main/java/com/study/springstudy/springmvc/util/LoginUtil.java
@@ -1,13 +1,22 @@
 package com.study.springstudy.springmvc.util;
 
+import com.study.springstudy.springmvc.chap05.dto.response.LoginUserInfoDto;
+
 import javax.servlet.http.HttpSession;
 
 public class LoginUtil {
 
-    public static final String LOGIN="login";
+    public static final String LOGIN = "login";
 
     // 로그인 여부 확인
-    public static boolean isLoggedIn(HttpSession session){
-        return session.getAttribute(LOGIN)!=null;
+    public static boolean isLoggedIn(HttpSession session) {
+        return session.getAttribute(LOGIN) != null;
     }
+
+    // 로그인한 회원의 계정명 얻기
+    public static String getLoggedInUserAccount(HttpSession session) {
+        LoginUserInfoDto currentUser = (LoginUserInfoDto) session.getAttribute(LOGIN);
+        return  (currentUser != null) ? currentUser.getAccount() : null;
+    }
+
 }
