67d2c49e602358df9ea813494a775b8d4cb7abca
kimbyeongmo <kbmomo9999@naver.com>
2024-06-04 14:32:30 +0900

    # 댓글인가처리 - 인터셉터 설정 및 fetch함수 호출 일원화

--- src/main/java/com/study/springstudy/springmvc/config/InterceptorConfig.java
+++ src/main/java/com/study/springstudy/springmvc/config/InterceptorConfig.java
@@ -1,6 +1,7 @@
 package com.study.springstudy.springmvc.config;
 
 import com.study.springstudy.springmvc.interceptor.AfterLoginInterceptor;
+import com.study.springstudy.springmvc.interceptor.ApiAuthInterceptor;
 import com.study.springstudy.springmvc.interceptor.AutoLoginInterceptor;
 import com.study.springstudy.springmvc.interceptor.BoardInterceptor;
 import lombok.RequiredArgsConstructor;
@@ -16,28 +17,38 @@ public class InterceptorConfig implements WebMvcConfigurer {
     private final AfterLoginInterceptor afterLoginInterceptor;
     private final BoardInterceptor boardInterceptor;
     private final AutoLoginInterceptor autoLoginInterceptor;
+    private final ApiAuthInterceptor apiAuthInterceptor;
 
     // 설정 메서드
-
-
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
 
         registry
                 .addInterceptor(afterLoginInterceptor)
                 // 해당 인터셉터가 동작할 URL을 설정
-                .addPathPatterns("/members/sign-up","/members/sign-in");
+                .addPathPatterns("/members/sign-up", "/members/sign-in")
+        ;
 
         // 게시판 인터셉터 등록
         registry
                 .addInterceptor(boardInterceptor)
                 .addPathPatterns("/board/*")
-                .excludePathPatterns("/board/list","/board/detail");
+                .excludePathPatterns("/board/list", "/board/detail"
+                        , "/board/like", "/board/dislike")
+        ;
 
         // 자동로그인 인터셉터 등록
         registry
                 .addInterceptor(autoLoginInterceptor)
-                .addPathPatterns("/**");
-    }
+                .addPathPatterns("/**")
+        ;
 
+        // REST API 인가 처리 인터셉터 등록
+        registry
+                .addInterceptor(apiAuthInterceptor)
+                .addPathPatterns("/api/v1/**")
+                .excludePathPatterns("/api/v1/replies/*/page/*")
+        ;
+
+    }
 }
